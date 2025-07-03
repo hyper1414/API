e8091ae22158d1dba4e6d1c3707c7443dff30075
Godwin Anorue <justmsnslone234@gmail.com>
2025-07-03 12:35:11 +0100

    added get by id

--- index.js
+++ index.js
@@ -33,7 +33,17 @@ app.get('/api/products', async (req, res) => {
     }
 });
 
-// app.delete('api/products')
+app.get('/api/products/:id', async (req, res) => {
+    
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 
 mongoose.connect
